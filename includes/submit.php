375387d6aa898bc0aa8c4f68f50a016d5cace204
0460208 <70180650+0460208@users.noreply.github.com>
2024-04-30 09:32:08 -0400

    update

--- includes/submit.php
+++ includes/submit.php
@@ -1,12 +1,26 @@
 <?php include "index.html"; ?>
 
-<?php 
-if(isset($_GET['submit'])) 
-{ 
-$to= "martel.amanda00@gmail.com"; 
-$subject= "Message to Amanda Martel"; 
-$msg= $_GET["value1"]; //textarea value will be send as message here. 
-$header= " " //For example: From: abc@xyz.com 
-mail($to,$subject,$msg,$header); 
-} 
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Collect form data
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+
+    // Compose email message
+    $to = 'martel.amanda00@gmail.com';
+    $subject = 'Contact Form Submission';
+    $body = "Name: $name\nEmail: $email\nMessage: $message";
+
+    // Send email
+    if (mail($to, $subject, $body)) {
+        echo "<p>Thank you for your message! We will get back to you shortly.</p>";
+    } else {
+        echo "<p>Oops! Something went wrong and we couldn't send your message.</p>";
+    }
+} else {
+    // Redirect to form page if accessed directly
+    header("Location: your_form_page.html");
+    exit;
+}
 ?>
