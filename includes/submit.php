60157e77a3cb4cb3f99bf628c360ab2ac99f7b21
0460208 <70180650+0460208@users.noreply.github.com>
2024-04-29 21:31:04 -0400

    update

---
+++ includes/submit.php
@@ -0,0 +1,10 @@
+<?php 
+if(isset($_POST['submit'])) 
+{ 
+$to= "martel.amanda00@gmail.com"; 
+$subject= "Message to Amanda Martel"; 
+$msg= $_POST["value1"]; //textarea value will be send as message here. 
+$header= " " //For example: From: abc@xyz.com 
+mail($to,$subject,$msg,$header); 
+} 
+?>
